455280741cc40bfc6e44fb3bb0f4774fd906551c
Juan Diego Valdivia Mendoza <jdiegodiego4@gmail.com>
2026-01-31 13:49:42 -0500

    .

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,15 @@
 {
     'name': 'Custom CRM PROPIFY PRO',
     'version': '1.0',
-    'depends': ['crm'],
+    'depends': ['base', 'crm', 'web'],
     'data': [
         'views/crm_views.xml',
     ],
     'installable': True,
+    'assets': {
+        'web.assets_backend': [
+            ('prepend', 'crm_custom_fields/static/src/scss/primary_variables.scss'),
+        ],
+    },
     'application': True,
 }
