e7d0045e46c1cff8237eee8a3da89b0f2613d505
Juan Diego Valdivia Mendoza <jdiegodiego4@gmail.com>
2026-01-31 13:52:41 -0500

    .

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -4,3 +4,9 @@ class CustomLead(models.Model):
     _inherit = 'crm.lead'
 
     id_externo = fields.Char(string='ID Transacción')
+    metodo_contacto = fields.Selection([
+        ('email', 'Email'),
+        ('phone', 'Teléfono'),
+        ('whatsapp', 'WhatsApp'),
+        ('other', 'Otro')
+    ], string='Método de Contacto')
